d2fed8ba11bc8573879c1ea04f5d8be8b4e8e8a8
José113df <josemateusti@gmail.com>
2024-09-25 14:19:09 -0300

    Entrega atividade 4

---
+++ Exo.WebApi/README.md
@@ -0,0 +1,4 @@
+## Atividade 3,4
+### Criar estrutura da API com Context e método GET para projetos
+### Criar o restante do CRUD de projetos, testando-o no Insomnia
+
--- Exo.WebApi/obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ Exo.WebApi/obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7c8ed73c77febcea44788e1388b79209274b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
